4948df91edc06544446244a6f80272e440b1b36b
Golam <rabby29hossain@gmail.com>
2023-08-13 22:49:53 -0400

    testing changes

--- src/assets/index.js
+++ src/assets/index.js
@@ -8,7 +8,6 @@ import menu from "./menu.svg"
 import close from "./close.svg"
 import demo from "./demo.jpg"
 
-//
 import css from "./tech/css.png"
 import docker from "./tech/docker.png"
 import figma from "./tech/figma.png"
